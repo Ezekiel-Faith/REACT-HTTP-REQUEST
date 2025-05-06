390fa3d9c557e1b07cb675169ba21fc89d1322e1
Ezekie-Faith <ezekielfaith2000@gmail.com>
2025-05-06 05:31:18 +0100

    sending data with post request(PUT)

--- src/http.js
+++ src/http.js
@@ -8,3 +8,21 @@ export async function fetchAvailablePlaces() {
 
   return resData.places;
 }
+
+export async function updateUserPlaces(places) {
+  const response = await fetch('http://localhost:3000/user-places', {
+    method: 'PUT',
+    body: JSON.stringify({ places }), // or it can be {places: places}, but left it that way since the key and value have the same name from the backend
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const resData = response.json();
+
+  if (!response.ok) {
+    throw new Error('Failed to update user data.');
+  }
+
+  return resData.message;
+}
